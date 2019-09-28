dcf5794480403e355d06b1354b7b6d7ff2bc3178
RubbaBoy <adamyarris@gmail.com>
2019-09-28 15:00:49 -0400

    Added demo module and structure

---
+++ demo/src/main/java/com/github/vroom/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.github.vroom.demo;
+
+import com.github.vroom.Vroom;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        var vroom = new Vroom();
+    }
+
+}
---
+++ src/main/java/com/github/vroom/Vroom.java
@@ -0,0 +1,12 @@
+package com.github.vroom;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Vroom {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(Vroom.class);
+
+
+
+}
